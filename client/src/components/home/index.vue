a3ac39e6d8dccaa731c441a7e699404b2662e5c4
koto <chanwei0912@gmail.com>
2023-03-09 12:29:29 +0800

    feat: new ui

--- client/src/components/home/index.vue
+++ client/src/components/home/index.vue
@@ -140,125 +140,115 @@ handleListUser();
 </script>
 
 <template>
-  <div class="w-screen h-screen p-4 bg-[#f0f0f0] flex flex-col relative">
-    <VoiceChat ref="chatRef" class="z-50" :sender-id="senderId"></VoiceChat>
-    <div class="flex flex-1 h-0 rounded">
-      <div class="user-list bg-[#fff] w-[300px] flex flex-col rounded-xl">
-        <div class="text-center py-4 border-b">
-          <span class="text-lg font-semibold">
+  <div class="w-screen h-screen bg-[#f0f0f0] flex relative">
+    <div class="left-box w-[74px] h-full bg-[#F9F9F9]"></div>
+
+    <div class="center-box w-[375px] border-r h-full bg-[#fff]">
+      <div class="profile mt-[30px] w-full flex flex-col justify-center items-center">
+        <div class="avatar w-[105px] h-[105px] rounded-full relative">
+          <div class="state-point w-[12px] h-[12px] rounded-full top-[85px] right-[12px] absolute bg-[#27AD55]"></div>
+          <img :src="getAvatarByUserId(user.id % 20)" class="w-[105px] h-[105px] rounded-full" alt="" srcset="" />
+        </div>
+        <div class="username mt-3">
+          <span class="text-xl font-medium">
             {{ user.username }}
           </span>
         </div>
-        <!-- <div class="p-2">
-          <span class="text-base font-semibold"># MESSAGE</span>
-        </div> -->
-        <div class="users-box flex-1">
-          <div
-            class="user relative cursor-pointer border-black-600 h-[50px] mb-2 flex items-center justify-between rounded"
-            v-for="(user, index) in users"
-            @click="setActiveUser(user, index)"
-            :key="index"
-            :class="activeUserIndex === index ? 'bg-[#f6f7fb]' : ''"
-          >
-            <div class="pic w-[40px] ml-2 relative h-[40px] rounded-full bg-slate-300">
-              <div
-                class="state absolute w-2 h-2 bottom-[1px] right-[4px] rounded-full"
-                :class="user.state === UserState.OFFLINE ? 'bg-red-500' : 'bg-green-500'"
-              ></div>
-              <img :src="getAvatarByUserId(user.id % 20)" class="w-[40px] h-[40px] rounded-full" alt="" srcset="" />
-            </div>
-            <div
-              class="info flex h-full w-0 flex-1 flex items-start border-b text-black ml-[12px]"
-              :class="activeUserIndex === index ? 'text-black' : 'text-black'"
-            >
-              <div class="flex flex-1 max-w-full flex-col">
-                <span class="text font-medium mt-1">{{ user.username }}</span>
-                <div class="text-xs text-[#cfcfcf] text-ellipsis whitespace-nowrap">
-                  {{ recentMapper[user.id] ? recentMapper[user.id].content : '' }}
-                </div>
-              </div>
-              <div class="w-[40px] h-full flex items-center justify-center">
-                <div
-                  class="bg-[#ebaa89] rounded-full h-4 w-4 flex justify-center text-center"
-                  v-show="recentMapper[user.id].newMsgCount"
-                >
-                  <span class="text-white text-xs">{{ recentMapper[user.id].newMsgCount }}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="setting p-4">
-          <!-- <div>
-            <span class="text-base font-semibold"># SETTING</span>
-          </div> -->
-          <div>
-            <button class="w-[100%] p-2 bg-blue-500 text-white rounded" @click="logout">Logout</button>
-          </div>
+        <div class="state-bar w-[68px] h-[22px] bg-[#C3EFD2] rounded mt-2 flex justify-center items-center">
+          <span class="text-xs text-[#2BB55A]">Active</span>
         </div>
       </div>
-      <div class="chat-box flex-1 ml-4 bg-[#fff] rounded-xl flex flex-col">
-        <div class="header h-[50px] border-b px-4 flex justify-between items-center">
-          <div class="h-full flex items-center">
-            <span class="text-lg font-semibold">{{ activeUser.username }}</span>
-          </div>
-          <div
-            class="cursor-pointer"
-            @click="handleRequestCall"
-            v-show="activeUser.id && activeUser.state === UserState.ONLINE"
-          >
-            <img :src="phoneCall" class="w-6 h-6" alt="" srcset="" />
-          </div>
+
+      <div class="chats-box">
+        <div class="header px-4 py-2">
+          <span class="text-xl font-medium">Chats</span>
         </div>
-        <div class="message-box flex-1 f-0 p-4 overflow-hidden">
-          <div class="h-full w-full overflow-y-auto p-2 mr-[10px]" ref="msgContainer" v-if="activeUser.id">
+        <div
+          class="chat-user px-4 py-2 h-[70px] w-full flex justify-between items-center"
+          :class="activeUserIndex === index ? 'bg-[#F6F6F6]' : null"
+          v-for="(u, index) in users"
+          @click="setActiveUser(u, index)"
+          :key="u.id"
+        >
+          <div class="w-[50px] h-[50px] relative rounded-full">
             <div
-              class="message-item mb-2 flex flex-col"
-              :class="msg.senderId === activeUser.id ? 'items-start' : 'items-end'"
-              v-for="(msg, index) in msgMapper[activeUser.id]"
-              :key="index"
-            >
-              <div class="msg-own">{{ msg.senderId === activeUser.id ? activeUser.username : user.username }}</div>
-              <div
-                class="msg-context rounded-b-xl bg-[#c9e5fb] px-4 py-2 min-h-[40px] max-w-[70%] whitespace-normal break-all"
-                :class="msg.senderId === activeUser.id ? 'rounded-tr-xl' : 'rounded-tl-xl'"
-              >
-                {{ msg.content }}
-              </div>
+              class="state-point w-[8px] h-[8px] rounded-full top-[36px] right-[6px] absolute"
+              :class="u.state === UserState.ONLINE ? ' bg-[#27AD55]' : ' bg-[#878B7C]'"
+            ></div>
+            <img :src="getAvatarByUserId(u.id % 20)" class="w-[50px] h-[50px] rounded-full" alt="" srcset="" />
+          </div>
+          <div class="ml-3 flex-1 w-0 h-full">
+            <div class="mb-1">
+              <span class="text-xl font-medium">{{ u.username }}</span>
+            </div>
+            <div class="text-xs text-[#A1A1A1] text-ellipsis whitespace-nowrap">
+              <span>{{ recentMapper[u.id] ? recentMapper[u.id].content : '' }}</span>
             </div>
           </div>
 
-          <!-- <div class="message-item mb-2 flex justify-end">
-            <div class="msg-own"></div>
-            <div class="msg-context bg-[#c9e5fb] p-2 min-h-[40px] max-w-[70%]"></div>
-          </div> -->
-        </div>
-        <div class="footer h-[60px] px-4 pb-4" v-show="activeUser.id">
-          <div class="send-box border h-full relative p-2 flex items-center">
-            <input
-              v-model="message"
-              :disabled="activeUser.state === UserState.OFFLINE"
-              type="text"
-              class="w-full h-full p-1 flex-1"
-              @keyup.enter="sendMessage"
-              :placeholder="sendInputPlaceholder"
-            />
-            <img
-              class="w-6 h-6 cursor-pointer"
-              :src="sendPic"
-              @click="
-                () => {
-                  if (activeUser.state === UserState.OFFLINE) {
-                    return;
-                  }
-                  sendMessage();
-                }
-              "
-              alt=""
-              srcset=""
-            />
+          <div class="h-full w-[40px]">
+            <div>
+              <span class="text-xs text-[#A1A1A1]">20:30</span>
+            </div>
+            <div
+              v-show="recentMapper[u.id].newMsgCount"
+              class="mt-1 flex text-center w-[18px] h-[18px] justify-center items-center rounded-full bg-[#A251E1]"
+            >
+              <span class="text-white text-xs">
+                {{ recentMapper[u.id].newMsgCount }}
+              </span>
+            </div>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="right-box flex-1 w-0 h-full flex flex-col">
+      <div class="header mt-[30px] mx-6 h-[35px] flex justify-between">
+        <div>
+          <span class="text-xl font-medium">{{ activeUser.username }}</span>
+        </div>
+        <div class="flex space-x-6">
+          <div class="w-[35px] h-[35px] rounded-lg bg-white"></div>
+          <div class="w-[35px] h-[35px] rounded-lg bg-white"></div>
+        </div>
+      </div>
+      <div class="message-box flex-1 h-0 w-full my-6 overflow-hidden">
+        <div class="h-full w-full overflow-y-auto px-4 mr-[10px]" ref="msgContainer" v-if="activeUser.id">
+          <div
+            class="message-item mb-3 flex"
+            :class="msg.senderId === activeUser.id ? 'items-start' : 'items-end flex-row-reverse'"
+            v-for="(msg, index) in msgMapper[activeUser.id]"
+            :key="index"
+          >
+            <div class="send-avatar w-[40px] h-[40px] rounded-full">
+              <img :src="getAvatarByUserId(msg.senderId)" class="w-[40px] h-[40px] rounded-full" alt="" srcset="" />
+            </div>
+
+            <!-- <div class="msg-own">{{ msg.senderId === activeUser.id ? activeUser.username : user.username }}</div> -->
+            <div
+              class="msg-context rounded-t-xl bg-[#c9e5fb] px-4 py-2 min-h-[40px] max-w-[70%] whitespace-normal break-all"
+              style="box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25)"
+              :class="
+                msg.senderId === activeUser.id ? 'rounded-br-xl ml-3 bg-[#fff]' : 'rounded-bl-xl mr-3 bg-[#D7FEE4]'
+              "
+            >
+              {{ msg.content }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="send-bar h-[74px] rounded-xl bg-white p-4 mx-6 mb-[40px]"
+        style="box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25)"
+      >
+        <input
+          v-model="message"
+          :disabled="activeUser.state === UserState.OFFLINE"
+          type="text"
+          class="w-full h-full p-1 flex-1"
+          @keyup.enter="sendMessage"
+          :placeholder="sendInputPlaceholder"
+        />
       </div>
     </div>
   </div>
@@ -271,5 +261,11 @@ input {
 
 input:focus {
   outline: none;
+}
+
+.profile .avatar {
+  background: #d9d9d9;
+  border: 2px solid #ffffff;
+  box-shadow: 4px 4px 6px rgba(197, 197, 197, 0.25);
 }
 </style>
